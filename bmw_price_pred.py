826e308cf4737b5b076eeb531cff5868e9b25bd7
Aryan Raj Singh <aryan@Aryans-MacBook-Air.local>
2026-03-24 20:33:44 +0800

    Added evaluation metric and optimized code

--- bmw_price_pred.py
+++ bmw_price_pred.py
@@ -1,49 +1,46 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 #Produce DataFrame from csv file
-bmw_data = pd.read_csv('FILEPATH')
-
+bmw_data = pd.read_csv('data/dataset.csv')
 
 #Converting data from words to 0/1
-def binary(df: pd.DataFrame) -> pd.DataFrame:
-    for item in df.transmission.tolist():
-        if item == 'Automatic':
-            df['transmission'].replace(item, 1, inplace = True)
-        else:
-            df['transmission'].replace(item, 0, inplace = True)
-    
-    for item in df.fuelType.tolist():
-        if item == 'Diesel':
-            df['fuelType'].replace(item, 1, inplace = True)
-        else:
-           df['fuelType'].replace(item, 0, inplace = True)
+def binarise(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.copy()
+    df['transmission'] = (df['transmission'] == 'Automatic').astype(int)
+    df['fuelType'] = (df['fuelType'] == 'Diesel').astype(int)
     return df
 
 #Final dataframe to fit and transform
-final_bmw_data = binary(bmw_data)
+final_bmw_data = binarise(bmw_data)
+model_column = final_bmw_data['model']
 final_bmw_data.drop(['model'], inplace=True, axis = 1)
 
 #Creating training and validating variables
-x = final_bmw_data.drop(['price'], axis=1)
+X = final_bmw_data.drop(['price'], axis=1)
 y = final_bmw_data['price']
-X_train, X_test, y_train, y_test = train_test_split(x, y,random_state=24)
+X_train, X_test, y_train, y_test = train_test_split(X, y,test_size = 0.3,random_state=24)
 
 #Initialise model
-model = RandomForestRegressor(n_estimators=1000, random_state=1)
+model = RandomForestRegressor(n_estimators=1000, random_state=1, n_jobs = -1)
 model.fit(X_train, y_train)
-y_pred = model.predict(x)
+y_pred = model.predict(X_test)
 
+#Evaluation
+mae  = mean_absolute_error(y_test, y_pred)
+print(f'Mean Absolute Error: {mae: .2f}')
 
 #Create Final df to compare values
 outputted_df = pd.DataFrame(
     {
-        'originalPrice': y,
-        'predictedPrice': y_pred
+        'originalPrice': y_test,
+        'predictedPrice': y_pred.round(0)
     }
 )
-outputted_df.index = pd.read_csv('/Users/aryan/Documents/Coding/VS_CODE/env/csv_files/bmw.csv')['model']
+outputted_df.index = model_column.iloc[X_test.index].values
+outputted_df.index.name = 'carModel'
 
 #Save df as new csv file
-outputted_df.to_csv('prediction-original-prices.csv')
+outputted_df.to_csv('data/model_predictions.csv')
